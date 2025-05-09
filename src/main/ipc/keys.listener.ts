63855784fc5ccb6871247b17ae4c8f3dcd501beb
Juan Andrés Carmena <juan14nob@gmail.com>
2025-05-09 13:25:01 -0300

    fix: code cleaning

--- src/main/ipc/keys.listener.ts
+++ src/main/ipc/keys.listener.ts
@@ -1,17 +1,10 @@
 import { ipcMain } from 'electron';
-import { dataInitializor, StoredData, StoreKey } from '../models';
+import { StoreKey } from '../models';
 import { eventBus } from './event-bus';
 import { IPC_CHANNELS } from './channels';
 import { addNewKey, getAllKeys, removeKey } from '../services/key.service';
 
-let storedData: StoredData = dataInitializor;
-
 export function registerKeyHandlers() {
-  const getData = async () => {
-    const loadedData = await localStorage.load();
-    storedData = loadedData ?? dataInitializor;
-    return storedData;
-  };
 
   ipcMain.handle(IPC_CHANNELS.ADD_KEY, async (_, newKey: StoreKey) => {
     await addNewKey(newKey);
@@ -27,15 +20,4 @@ export function registerKeyHandlers() {
     return await getAllKeys();
   });
 
-  /*
-  ipcMain.handle('delete-key', async (_, keyId) => {
-    const result = await localStorage.deleteKey(keyId);
-    //await sync();
-    return result;
-  });
-
-  ipcMain.handle('load-keys', async () => {
-    return await getData();
-  });
-  */
 }
